06b169159c2274c6f79058be9fa8997a51ec0a87
Jay Wiggins <jay@irk.com>
2021-03-31 15:18:39 -0700

    first check in

---
+++ secrets.py
@@ -0,0 +1,8 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'Koolbreeze Ranch U',
+    'password' : 'duchamp1',
+    'timezone' : "America/Phoenix", # http://worldtimeapi.org/timezones
+    }
